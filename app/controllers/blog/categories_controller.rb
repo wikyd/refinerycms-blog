f4f2980190c9fa1615a8f726522d2314ed224c18
Raphael Crawford-Marks <raphael.crawfordmarks@gmail.com>
2012-04-16 14:46:41 -0400

    Permanently redirect if not best friendly id.

--- app/controllers/blog/categories_controller.rb
+++ app/controllers/blog/categories_controller.rb
@@ -2,7 +2,7 @@ class Blog::CategoriesController < BlogController
 
   def show
     @category = BlogCategory.find(params[:id])
-    if !params[:page].blank? && params[:page].to_i == 1
+    if !@category.friendly_id_status.best? || (!params[:page].blank? && params[:page].to_i == 1)
       return permanent_redirect_to(blog_category_url(@category))
     end
     @blog_posts = @category.posts.live.includes(:comments, :categories).paginate({
